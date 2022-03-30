41371ffce46f087315c3fbead2f427fd08fcf20b
skyo <1770834230@qq.com>
2022-03-30 15:39:08 +0800

    成功全局引入markdown编辑器mavonEditor

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,24 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+
 import router from "./router";
+
 import { createPinia } from "pinia";
+
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 import zhCn from "element-plus/es/locale/lang/zh-cn";
 
+import mavonEditor from "mavon-editor";
+import "mavon-editor/dist/css/index.css";
+
 import "./assets/css/global.less";
+
 import "./assets/css/font-awesome.min.css";
 import "animate.css";
 
 createApp(App)
+  .component("Editor", mavonEditor.mavonEditor)
   .use(ElementPlus, {
     locale: zhCn,
   })
--- src/views/userPage/Blog.vue
+++ src/views/userPage/Blog.vue
@@ -1,10 +1,32 @@
 <template>
   <div class="blog">
     <p>博客页面</p>
+    <el-button @click="newBlog">开始创作</el-button>
+    <el-dialog
+      top="5vh"
+      v-model="dialogVisible"
+      title="博客创作"
+      center
+      width="80%"
+    >
+      <div style="height: 70vh; overflow: auto">
+        <el-scrollbar>
+          <Editor style="height: 70vh; width: 99%"></Editor>
+        </el-scrollbar>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// import Editor from "../../components/Editor.vue";
+import { ref } from "vue";
+
+const dialogVisible = ref(false);
+const newBlog = () => {
+  dialogVisible.value = true;
+};
+</script>
 
 <style scoped>
 .blog {
